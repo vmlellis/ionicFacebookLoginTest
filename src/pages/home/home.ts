c691d09a4e502da084568c5e963982876ec7c13c
Victor Lellis <vmlellis@gmail.com>
2018-01-17 19:23:59 -0200

    Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isLoggedIn:boolean = false;
+  user: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private fb: Facebook) {
+    fb.getLoginStatus()
+      .then(res => {
+        console.log(res.status);
+        if(res.status === "connect") {
+          this.isLoggedIn = true;
+        } else {
+          this.isLoggedIn = false;
+        }
+      })
+      .catch(e => console.log(e));
   }
 
+  login() {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then(res => {
+        if(res.status === "connected") {
+          this.isLoggedIn = true;
+          this.getUserDetail(res.authResponse.userID);
+        } else {
+          this.isLoggedIn = false;
+        }
+      })
+      .catch(e => console.log('Error logging into Facebook', e));
+  }
+
+  logout() {
+    this.fb.logout()
+      .then( res => this.isLoggedIn = false)
+      .catch(e => console.log('Error logout from Facebook', e));
+  }
+
+  getUserDetail(userid) {
+    this.fb.api("/"+userid+"/?fields=id,email,name,picture,gender",["public_profile"])
+      .then(res => {
+        console.log(res);
+        this.user = res;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
 }
